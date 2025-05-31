272610a9884eebea4ab50a709b23ef4b9c11cf92
Im-Delfianov <delfianov@gmail.com>
2025-05-31 12:58:04 +0700

    revisi2deployprob

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -5,4 +5,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; // gunakan service role untuk backend
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase environment variables.');
+}
 module.exports = supabase;
