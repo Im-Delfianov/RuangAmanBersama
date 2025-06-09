693f9d94798f7ce56da08cb14e14b73976da1510
Im-Delfianov <delfianov@gmail.com>
2025-06-09 10:48:50 +0700

    charlimit

--- src/middlewares/charCounterMiddleware.js
+++ src/middlewares/charCounterMiddleware.js
@@ -4,11 +4,11 @@ const { title, content } = req.body;
   const contentCharCount = content.length;
 
   if (titleCharCount > 100) {
-    return res.status(401).json({ message: 'Judul tidak boleh lebih dari 500 karakter' });
+    return res.status(413).json({ message: 'Judul tidak boleh lebih dari 500 karakter' });
   }
 
-  if (contentCharCount > 500) {
-    return res.status(401).json({ message: 'Isi tidak boleh lebih dari 500 karakter' });
+  if (contentCharCount > 1500) {
+    return res.status(413).json({ message: 'Isi tidak boleh lebih dari 1500 karakter' });
   }
   
   next();
@@ -18,8 +18,8 @@ exports.commentCharCounter = (req, res, next) =>{
 const {content } = req.body;
   const contentCharCount = content.length;
 
-  if (contentCharCount > 500) {
-    return res.status(401).json({ message: 'Isi tidak boleh lebih dari 500 karakter' });
+  if (contentCharCount > 1500) {
+    return res.status(413).json({ message: 'Isi tidak boleh lebih dari 1500 karakter' });
   }
   
   next();
