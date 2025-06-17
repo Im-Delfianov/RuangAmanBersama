ac41266959df4f01d9574ffc1bc3b9c76d065988
Im-Delfianov <delfianov@gmail.com>
2025-06-17 07:48:37 +0700

    googleLogin

--- src/utils/cleanUpUnverified.js
+++ src/utils/cleanUpUnverified.js
@@ -1,12 +1,13 @@
 const cron = require('node-cron');
 const pool = require('../config/database');
+const dayjs = require('dayjs');
 
 
 cron.schedule('0 * * * *', async () => {
 
   console.log("🕒 Running cron job at", new Date());
   try {
-    const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000); // 1 jam yang lalu
+    const oneHourAgo = dayjs().subtract(1, 'hour').toDate();
     const result = await pool.query(
       'DELETE FROM public.users WHERE is_verified = false AND created_at < $1',
       [oneHourAgo]
